b127958f45c21b604f910f2ff7eb67f6e479d254
Yared Bekele <yaredworku@gmail.com>
2025-06-30 11:54:16 +0200

    Update package documentation to clarify data preprocessing and spatial alignment details

--- src/terrain_change_detection/__init__.py
+++ src/terrain_change_detection/__init__.py
@@ -3,6 +3,10 @@ Terrain Change Detection Package
 
 A Python package for terrain change detection from multi-temporal point cloud data.
 This package provides tools for data preprocessing, spatial alignment and change detection.
+Data preprocessing is optimized for LiDAR point cloud data from hoydedata.no.
+Spatial alignment is implemented using the Iterative Closest Point (ICP) algorithm. For a fine-grained
+control of the spatial alignment, the ICP algorithm is implemented from scratch.
+Change detection is a WIP...(will most likely use py4dgeo)
 """
 
 __version__ = "0.1.0"
